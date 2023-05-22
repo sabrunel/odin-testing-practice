12320ceafd49856180d13c63a1026a526fffd63d
Samantha Brunel <59365424+sabrunel@users.noreply.github.com>
2023-05-22 20:15:59 +0200

    Write unit tests for the capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,17 @@
-function capitalize(word) {
-    return word;
+function capitalize(word) {  
+    const firstLetter = word[0];
+    
+    if (isSpecialCharacter(firstLetter) || !isNaN(firstLetter)) {
+        throw new Error("Input string must start with a letter");
+    }
+
+    return firstLetter.toUpperCase() + word.slice(1).toLowerCase();
 }
 
+function isSpecialCharacter(input) {
+    const specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    return specialCharacters.test(input);
+}
+    
 module.exports = capitalize
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,35 @@
 const capitalize = require("./capitalize");
 
-it("works", () => {
-    expect(capitalize("banana")).toBe("banana");
+it("Returns a capitalized string when a lowercase string is supplied", () => {
+    expect(capitalize("banana")).toBe("Banana");
+})
+
+it("Returns a capitalized string when an uppercase string is supplied", () => {
+    expect(capitalize("BANANA")).toBe("Banana");
+})
+
+it("Returns a capitalized string when a string that mixes lowercase and uppercase characters is supplied", () => {
+    expect(capitalize("BaNaNa")).toBe("Banana");
+})
+
+it("Returns the input string when a capitalized string is supplied", () => {
+    expect(capitalize("Banana")).toBe("Banana");
+})
+
+it("Throws an error if the input is an empty string", () => {
+    expect(() => {
+        capitalize("")
+    }).toThrow();
+})
+
+it("Throws an error if the input string starts with a special character", () => {
+    expect(() => {
+        capitalize("!Banana")
+    }).toThrow();
+})
+
+it("Throws an error if the input string starts with a number", () => {
+    expect(() => {
+        capitalize("1Banana")
+    }).toThrow();
 })
