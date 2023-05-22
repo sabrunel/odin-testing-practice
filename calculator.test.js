f84757659fe5453d7184984dd8e73acc633ade07
Samantha Brunel <59365424+sabrunel@users.noreply.github.com>
2023-05-22 22:56:09 +0200

    Write unit tests for the calculator object's functions

---
+++ calculator.test.js
@@ -0,0 +1,23 @@
+const calculator = require("./calculator");
+
+it("Adds two numbers", () => {
+    expect(calculator.add(1,2)).toEqual(3);
+})
+
+it("Subtracts two numbers", () => {
+    expect(calculator.subtract(1,2)).toEqual(-1);
+})
+
+it("Divides two numbers", () => {
+    expect(calculator.divide(1,2)).toEqual(0.5);
+})
+
+it("Multiplies two numbers", () => {
+    expect(calculator.multiply(1,2)).toEqual(2);
+})
+
+it("Throws an error when dividing by 0", () => {
+    expect(() => {
+        calculator.divide(1,0)
+    }).toThrow("Cannot divide by 0");
+})
